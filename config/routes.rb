98c4374df57655a646055eaae014272bcade3700
Filonov Igor <filonovia@gmail.com>
2020-11-28 22:28:10 +0500

    debise g male female

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :females
+  devise_for :males
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "application#index"
 end
