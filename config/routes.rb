ff8dfcf8876fd4dabc09a5347f0608a3bdfc72cd
Filonov Igor <filonovia@gmail.com>
2020-11-28 22:29:26 +0500

    devise g staff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :staffs
   devise_for :females
   devise_for :males
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
