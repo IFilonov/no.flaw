7eec0004a21725c2d6318942090ab7f9319bdedf
Filonov Igor <filonovia@gmail.com>
2020-12-01 23:18:37 +0500

    add layoutes, vue templates, pug

---
+++ app/javascript/noflaw/components/staff/staff.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    q-layout
+      h1 Staff vue
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted() {
+  }
+}
+</script>
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
   devise_for :females, controllers: { sessions: 'females/sessions' }
   devise_for :males, controllers: { sessions: 'males/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  authenticated :male do
+    root 'males#index', as: :authenticated_male
+  end
+  authenticated :females do
+    root 'females#index', as: :authenticated_female
+  end
   root to: "application#index"
+  get '/males/*slug', to: 'males#index'
+  get '/staffs/*slug', to: 'staffs#index'
+  get '/females/*slug', to: 'females#index'
 end
