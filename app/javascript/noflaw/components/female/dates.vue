f0cc8eee84a730b03b2673d0b3b1f0d2cffcc0b2
Filonov Igor <filonovia@gmail.com>
2021-01-09 02:56:22 +0500

    json.stringify dates

--- app/javascript/noflaw/components/female/dates.vue
+++ app/javascript/noflaw/components/female/dates.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
-  div(class="q-pa-md q-gutter-sm")
+  div(class="q-pa-md")
     q-badge(color="purple" text-color="white" class="q-ma-md") Mask: DD-MM-YYYY HH:mm
     div(class="q-gutter-md row items-start")
       q-date(v-model="dates" mask="DD-MM-YYYY HH:mm" multiple range color="purple" @input="onInput")
       q-time(v-model="dates" mask="DD-MM-YYYY HH:mm" color="purple")
     p(v-for="date in dates")
-      q-badge(color="teal") {{ date }}
+      q-badge(color="teal") {{ JSON.stringify(date) }}
 </template>
 
 <script>
@@ -22,10 +22,15 @@ export default {
   },
   methods: {
     ...mapActions(['setMaleName']),
-    onInput(value, reason, details){
-      console.log(value)
-      console.log(reason)
-      console.log(details)
+    onInput(dates, reason, details){
+      let datesStr = dates.map(date => JSON.stringify(date));
+      let datesObj = datesStr.map(date => JSON.parse(date));
+      console.log(dates)
+      console.log(datesStr)
+      console.log(datesObj)
+      // console.log(value)
+      // console.log(reason)
+      // console.log(details)
     }
   },
   computed: {
