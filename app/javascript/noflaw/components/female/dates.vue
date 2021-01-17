687c2baa0b630cbeb4ff60c09d986eb66c55e737
Filonov Igor <filonovia@gmail.com>
2021-01-18 00:47:56 +0500

    add fire change

--- app/javascript/noflaw/components/female/dates.vue
+++ app/javascript/noflaw/components/female/dates.vue
@@ -1,13 +1,12 @@
 <template lang="pug">
-  div(class="q-pa-md") Bad dates
-    q-badge(color="purple" text-color="white" class="q-ma-md") Mask: DDMMYYYY
+  div(class="q-pa-md")
+    q-btn(push color="white" text-color="purple" class="text-bold" disable flat label="Taboo dates")
+      q-badge(color="purple" floating) {{ taboo_dates.length }}
     div(class="q-gutter-md row items-start")
-      q-date(v-model="dates" mask="DDMMYYYY" multiple today-btn first-day-of-week="1" range color="purple" @input="onInput")
-      q-time(v-model="dates" mask="DD-MM-YYYY HH:mm" color="purple")
-    div(class="q-pa-md")
-      q-btn(class="glossy" label="Send" @click="sendTabooDate" color="primary" :disable="disableSaveButton" v-close-popup icon="card_giftcard")
-      p(v-for="date in dates")
-        q-badge(color="teal") {{ JSON.stringify(date) }}
+      q-date(v-model="dates" mask="DDMMYYYY" multiple today-btn first-day-of-week="1" range color="purple" @input="onDateChange")
+      q-time(v-model="times" mask="DD-MM-YYYY HH:mm" color="purple" @input="onTimeChange")
+    br
+    q-btn(class="glossy" label="Send" @click="sendTabooDate" color="purple" :disable="disableSaveButton" v-close-popup icon="card_giftcard")
 </template>
 
 <script>
@@ -20,14 +19,18 @@ export default {
   data: function () {
     return {
       dates: [],
+      times: [],
       disableSaveButton: true
     }
   },
   methods: {
     ...mapActions(['setMaleName','setTabooDates','tabooDates']),
-    onInput(dates, reason, details){
+    onDateChange(dates, reason, details){
       this.disableSaveButton = false;
       this.setTabooDates(dates);
+    },
+    onTimeChange(times, reason, details){
+      console.log(times);
     },
     async sendTabooDate() {
       let tabooDate = JSON.stringify(this.taboo_dates.map(date => JSON.stringify(date)));
