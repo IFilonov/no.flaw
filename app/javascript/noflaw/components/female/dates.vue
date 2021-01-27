4ef03be4fe1bb8f69de1417553c04840aa1b42ce
Filonov Igor <filonovia@gmail.com>
2021-01-28 02:54:00 +0500

    check range component

--- app/javascript/noflaw/components/female/dates.vue
+++ app/javascript/noflaw/components/female/dates.vue
@@ -1,41 +1,24 @@
 <template lang="pug">
-  div(class="q-pa-md")
-    span(class="q-pa-md")
+  span(class="q-pa-md")
+    span(class="q-pa-md q-gutter-sm")
       q-btn(push color="white" text-color="purple" class="text-bold" disable flat label="Taboo dates")
         q-badge(color="purple" floating) {{ taboo_dates ? taboo_dates.length : 0 }}
       div(class="q-gutter-md row items-start")
         q-date(v-model="taboo" :options="tabooOptionsFn" multiple today-btn first-day-of-week="1" color="purple" @input="onTabooChange")
       br
       q-btn(class="glossy" label="Send" @click="sendTabooDate" color="purple" :disable="disableSaveTaboo" v-close-popup icon="card_giftcard")
-    span(class="q-pa-md")
+      q-btn(class="glossy" label="Clear" @click="clearTabooDate" color="purple" :disable="!Array.isArray(taboo)" v-close-popup icon="card_giftcard")
+    span(class="q-gutter-sm")
       q-btn(push color="white" text-color="deep-orange" class="text-bold" disable flat label="Fire dates")
         q-badge(color="deep-orange" floating) {{ fire_dates ? fire_dates.length : 0 }}
       div(class="q-gutter-md row items-start")
         q-date(v-model="fire" :options="fireOptionsFn" multiple today-btn first-day-of-week="1" color="deep-orange" @input="onFireChange")
       br
       q-btn(class="glossy" label="Send" @click="sendFireDate" color="deep-orange" :disable="disableSaveFire" v-close-popup icon="card_giftcard")
+      q-btn(class="glossy" label="Clear" @click="clearFireDate" color="deep-orange" :disable="!Array.isArray(fire)" v-close-popup icon="card_giftcard")
     span(class="q-pa-md" style="vertical-align: top;")
-      range
-      range
-      range
-      q-btn(push color="white" text-color="deep-orange" class="text-bold" disable flat label="Fire time")
-      div(class="q-gutter-sm")
-        q-checkbox(keep-color v-model="saveTime" label="Time" color="deep-orange")
-      q-range(v-model="time"
-        class="q-range"
-        left-label-color="deep-orange"
-        right-label-color="deep-orange"
-        :left-label-value="time.min + 'h'"
-        :right-label-value="time.max + 'h'"
-        :min="range.min"
-        :max="range.max"
-        :step="range.step"
-        label-always
-        vertical
-        markers
-        :disable="!saveTime"
-        drag-range
-        color="deep-orange")
+      div(v-for="(date) in fire")
+        range(:date = "date")
 </template>
 
 <script>
@@ -74,21 +57,27 @@ export default {
     },
     onFireChange(dates, reason, details){
       this.disableSaveFire = false;
-      console.log(this.time);
+      console.log(this.fire);
       this.setFireDate(dates);
     },
     async sendTabooDate() {
       console.log(this.taboo);
       let tabooDate = this.taboo_dates ? JSON.stringify(this.taboo_dates.map(date => JSON.stringify(date))) : null;
       const response = await this.$api.female.setTabooDate({ taboo_dates: tabooDate } );
-      response.data.created_at ? this.showNotif(`Taboodates saved at ${response.data.created_at}`)
+      response.data.created_at ? this.showNotif(`Taboodates saved at ${response.data.created_at}`, 'purple')
           : this.showErrNotif(response.data[0]);
     },
     async sendFireDate() {
       let fireDate = this.fire_dates ? JSON.stringify(this.fire_dates.map(date => JSON.stringify(date))) : null;
       const response = await this.$api.female.setFireDate({ fire_dates: fireDate } );
-      response.data.created_at ? this.showNotif(`Firedates saved at ${response.data.created_at}`)
+      response.data.created_at ? this.showNotif(`Firedates saved at ${response.data.created_at}`, 'deep-orange')
           : this.showErrNotif(response.data[0]);
+    },
+    clearFireDate() {
+      this.fire = null
+    },
+    clearTabooDate() {
+      this.taboo = null
     },
     fireOptionsFn(date) {
       return !this.taboo?.includes(date);
@@ -112,10 +101,5 @@ export default {
   span {
     display: inline-block;
     height: 200px;
-  }
-  .q-range {
-    height: 338px;
-    background-size: 100% 2px, 100% 2px, var(--tick-bg-size);
-    border: 4px solid;
   }
 </style>
--- app/javascript/noflaw/components/female/range.vue
+++ app/javascript/noflaw/components/female/range.vue
@@ -1,27 +1,25 @@
 <template lang="pug">
-  div
-    q-btn(push color="white" text-color="deep-orange" class="text-bold" disable flat label="Fire time")
-      div(class="q-gutter-sm")
-        q-checkbox(keep-color v-model="saveTime" label="Time" color="deep-orange")
-    q-range(v-model="time"
-      class="q-range"
-      left-label-color="deep-orange"
-      right-label-color="deep-orange"
-      :left-label-value="time.min + 'h'"
-      :right-label-value="time.max + 'h'"
-      :min="range.min"
-      :max="range.max"
-      :step="range.step"
-      label-always
-      vertical
-      markers
-      :disable="!saveTime"
-      drag-range
-      color="deep-orange")
+  span(class="range")
+    q-checkbox(keep-color v-model="saveTime" :label="date" color="deep-orange" text-color="deep-orange" style="align: left;")
+      q-range(v-model="time"
+        class="q-range"
+        left-label-color="deep-orange"
+        right-label-color="deep-orange"
+        :left-label-value="time.min + 'h'"
+        :right-label-value="time.max + 'h'"
+        :min="range.min"
+        :max="range.max"
+        :step="range.step"
+        label-always
+        markers
+        :disable="!saveTime"
+        drag-range
+        color="deep-orange")
 </template>
 
 <script>
 export default {
+  props: ['date'],
   data: function () {
     return {
       range: {
@@ -33,8 +31,30 @@ export default {
         min: 5,
         max: 7
       },
-      saveTime: false
+      saveTime: false,
+      nextNum: 10
     }
-  }
+  },
 }
 </script>
+
+<style scoped>
+.q-range {
+  width: 400px;
+  border: 3px solid;
+}
+.range {
+  height: 64px;
+}
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>
